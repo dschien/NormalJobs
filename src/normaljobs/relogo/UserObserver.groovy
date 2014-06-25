66cef351683115a3790e070d49b8e042bf88cd83
Daniel Schien <dschien@gmail.com>
2014-06-25 11:41:58 +0100

    phase 3

--- src/normaljobs/relogo/UserObserver.groovy
+++ src/normaljobs/relogo/UserObserver.groovy
@@ -29,19 +29,38 @@ class UserObserver extends ReLogoObserver{
 
 	@Go
 	def go(){
+		int idleWorkers = 0
+
 		ask(workers()){ worker ->
 			if (!worker.job) {
 				if (this.openJobs){
-					worker.job = this.openJobs.poll() 
-				} 
-			} 
+					worker.job = this.openJobs.poll()
+				} else {
+					// all jobs done
+					idleWorkers ++
+
+				}
+			}
 			// let the worker work on it
 			worker.step()
-			
+
 			if (worker.isJobDone()){
 				this.doneJobs << worker.job
 				worker.job = null
 			}
-		}		
+		}
+		// we've completed our work
+		if (idleWorkers == numWorkers){
+			RunEnvironment.getInstance().endRun();
+		}
 	}
+
+	def getNumOpenJobs(){
+		return this.openJobs.size()
+	}
+
+	def getNumDoneJobs(){
+		return this.doneJobs.size()
+	}
+
 }
