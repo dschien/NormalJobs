8e06d5b689098746b6f3380ff36cba0826e21ac0
Daniel Schien <dschien@gmail.com>
2014-06-25 12:04:11 +0100

    phase 4

--- src/normaljobs/relogo/UserGlobalsAndPanelFactory.groovy
+++ src/normaljobs/relogo/UserGlobalsAndPanelFactory.groovy
@@ -6,6 +6,8 @@ public class UserGlobalsAndPanelFactory extends AbstractReLogoGlobalsAndPanelFac
 	public void addGlobalsAndPanelComponents(){
 		
 		addSliderWL ("numWorkers", "Number of Workers", 1 , 1 , 1000 , 11)
-		addSliderWL ("numJobs", "Number of Jobs", 1 , 1 , 1000 , 1000)
+		addSliderWL ("numRequirements", "Number of Requirements", 1 , 1 , 10000 , 1000)
+        addSliderWL ("numberSteps", "Number of Steps", 1 , 1 , 100 , 50)
+        addSliderWL ("workIncrement", "Requirements solved per step", 1 , 1 , 100 , 10)
 	}
 }
